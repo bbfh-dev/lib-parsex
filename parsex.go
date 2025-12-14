bc59f6d8c46b15e4a0883b9303eb578bfa01eab7
BubbleFish <daforsastudia@gmail.com>
2025-12-14 18:15:16 +0300

    feat: Add input parsing placeholder

--- parsex.go
+++ parsex.go
@@ -1,5 +1,13 @@
 package libparsex
 
+import (
+	"errors"
+	"os"
+)
+
+// Return this error in [EntryPoint] to print help message and exit.
+var PrintHelpErr = errors.New("--help")
+
 func Run(program *Program, args []string) error {
 	if !program.didParse {
 		if err := program.Parse(); err != nil {
@@ -7,7 +15,23 @@ func Run(program *Program, args []string) error {
 		}
 	}
 
-	// TODO: Parse args
+	if err := parseInput(program, args); err != nil {
+		return err
+	}
 
-	return program.EntryPoint()
+	err := program.EntryPoint()
+	if err == PrintHelpErr {
+		program.PrintHelp(os.Stdout)
+		return nil
+	}
+
+	return err
+}
+
+func parseInput(program *Program, args []string) error {
+	if len(args) == 0 {
+		return nil
+	}
+
+	return nil
 }
